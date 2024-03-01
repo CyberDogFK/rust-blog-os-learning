04a1e253167a61301ab2281271db02c4a8ad3748
Anton Pavliuk <streetreadger@gmail.com>
2024-03-02 00:30:10 +0200

    Add writing to the screen writer interface

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
+mod vga_buffer;
 
 use core::panic::PanicInfo;
 
@@ -9,20 +10,19 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-static HELLO: &[u8] = b"Hello World!";
-
 #[no_mangle] // don't mange the name of this function
 pub extern "C" fn _start() -> ! {
     // this function is the entry point, since the linker looks for a function
     // named `_start` by default
-    let vga_buffer = 0xb8000 as  *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    use core::fmt::Write;
+    vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
+    write!(
+        vga_buffer::WRITER.lock(),
+        ", some numbers: {}, {}",
+        42,
+        1.337
+    )
+    .unwrap();
 
     loop {}
 }
