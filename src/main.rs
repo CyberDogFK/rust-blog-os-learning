ab44f0f8bc88928d6adc0b9b2e355a358537e670
Anton Pavliuk <streetreadger@gmail.com>
2024-03-03 11:54:24 +0200

    Add printing macro

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,8 @@ use core::panic::PanicInfo;
 
 /// This function is called on panic.
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
@@ -14,15 +15,8 @@ fn panic(_info: &PanicInfo) -> ! {
 pub extern "C" fn _start() -> ! {
     // this function is the entry point, since the linker looks for a function
     // named `_start` by default
-    use core::fmt::Write;
-    vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
-    write!(
-        vga_buffer::WRITER.lock(),
-        ", some numbers: {}, {}",
-        42,
-        1.337
-    )
-    .unwrap();
+    println!("Hello world{}", "!");
+    panic!("Some panic message");
 
     loop {}
 }
