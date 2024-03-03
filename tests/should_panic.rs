23795f9640fae3cc6ece6904689427aa2dc5567f
Anton Pavliuk <streetreadger@gmail.com>
2024-03-03 14:53:42 +0200

    Disable harness (need of test runner) for should panic
    It's work because should panic have only one test function and don't
    need for real test runner

--- tests/should_panic.rs
+++ tests/should_panic.rs
@@ -1,27 +1,16 @@
 #![no_std]
 #![no_main]
-#![feature(custom_test_frameworks)]
-#![test_runner(test_runner)]
-#![reexport_test_harness_main = "test_main"]
 
 use blog_os::{exit_qemu, serial_print, serial_println, QemuExitCode};
 use core::panic::PanicInfo;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    test_main();
+    should_fail();
+    serial_print!("[test did not panic]");
+    exit_qemu(QemuExitCode::Failed);
 
     loop {}
-}
-
-pub fn test_runner(tests: &[&dyn Fn()]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test();
-        serial_println!("[test did not panic]");
-        exit_qemu(QemuExitCode::Failed);
-    }
-    exit_qemu(QemuExitCode::Success)
 }
 
 #[panic_handler]
@@ -31,7 +20,6 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-#[test_case]
 fn should_fail() {
     serial_print!("should_panic::should_fail...\t");
     assert_eq!(0, 1);
