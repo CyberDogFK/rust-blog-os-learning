32504a8c0e8b62636269d7fd280d5fc706b75041
Anton Pavliuk <streetreadger@gmail.com>
2024-03-09 12:21:22 +0200

    Working on implementation of memory page mapping

--- src/main.rs
+++ src/main.rs
@@ -4,22 +4,40 @@
 #![test_runner(blog_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-use blog_os::println;
+use blog_os::{memory, println};
+use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
+use x86_64::structures::paging::Translate;
+use x86_64::VirtAddr;
 
-#[no_mangle] // don't mange the name of this function
-pub extern "C" fn _start() -> ! {
+entry_point!(kernel_main);
+
+pub fn kernel_main(boot_info: &'static BootInfo) -> ! {
     // this function is the entry point, since the linker looks for a function
     // named `_start` by default
     println!("Hello world{}", "!");
 
     blog_os::init();
 
-    // fn stack_overflow() {
-    //     stack_overflow();
-    // }
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mapper = unsafe { memory::init(phys_mem_offset) };
 
-    // stack_overflow();
+    let addresses = [
+        // the identity-mapped vga buffer page
+        0xb8000,
+        // some code page
+        0x201008,
+        // some stack page
+        0x0100_0020_1a10,
+        // virtual address mapped to physical address 0
+        boot_info.physical_memory_offset,
+    ];
+
+    for &address in &addresses {
+        let virt = VirtAddr::new(address);
+        let phys = mapper.translate_addr(virt);
+        println!("{:?} -> {:?}", virt, phys);
+    }
 
     // println!("Hello better world{}", "!");
     #[cfg(test)]
